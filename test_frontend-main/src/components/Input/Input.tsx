c909c6f81c6802d0f812a47dbc57dd5a36567a26
Adrian Peso <adrian.peso96@gmail.com>
2024-04-15 15:42:31 +0200

    feat(front): add password toggle visibility and header transition styles

--- test_frontend-main/src/components/Input/Input.tsx
+++ test_frontend-main/src/components/Input/Input.tsx
@@ -1,7 +1,9 @@
-import React, { FC, LegacyRef, MouseEventHandler, ReactElement, useRef } from 'react';
+import React, { FC, MouseEventHandler, MouseEvent, ReactElement, useRef, useState } from 'react';
 import styles from './Input.module.scss';
 import ErrorInterface from '../../types/error';
 import { useTranslation } from 'react-i18next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 interface InputProps {
     type: string;
@@ -19,23 +21,33 @@ interface InputProps {
     maxlength?: string;
 }
 
-const Input: FC<InputProps> =  ({ label, tooltip, inline , inverted, onChange, error, ...rest }) => {
+const Input: FC<InputProps> =  ({ type, label, tooltip, inline , inverted, onChange, error, ...rest }) => {
     const inputRef = useRef<HTMLInputElement>(null);
     const {t} = useTranslation();
+
+    const isPassword = type === 'password';
+    const  [passwordVisibility, setPasswordVisibility ] = useState(false);
 
     return (
         <div className={`${styles.input} ${inline && styles.inline} ${inverted && styles.inverted}`}>
             <label className="">{label}{tooltip}</label>
-            <input
-                ref={inputRef}
-                className={ error?.isError ? styles.error : ""} 
-                onChange={(e) => {
-                    onChange?.(e.target.value);
-                }}
-                
-                {...rest}
-            />
-            {error?.isError && <span> {t(error?.message) }</span>}
+            <div className={styles.input__inputContainer}>
+                <input
+                    type= {passwordVisibility ? 'text' : type}
+                    ref={inputRef}
+                    className={ error?.isError ? styles.error : ""} 
+                    onChange={(e) => {
+                        onChange?.(e.target.value);
+                    }}
+                    
+                    {...rest}
+                />
+                {isPassword && <button className={styles.input__toggleVisibility} onClick={ (ev : MouseEvent) => {
+                        ev.preventDefault();
+                        setPasswordVisibility(!passwordVisibility)
+                    }}><FontAwesomeIcon icon={ passwordVisibility ? faEyeSlash : faEye} /></button>}
+            </div>
+            {error?.isError && <span className={styles.errorText}> {t(error?.message) }</span>}
             {rest.maxlength && <span className={styles.input__counter}> {inputRef.current?.value.length}/{rest.maxlength}</span>}
         </div>
     );
